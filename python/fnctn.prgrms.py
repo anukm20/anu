fd2e849a10b09768112ecef4cbf6e6d8ebeb34e1
anurag <anuanurag248@gmail.com>
2023-10-19 09:13:13 +0530

    file added

---
+++ python/fnctn.prgrms.py
@@ -0,0 +1,56 @@
+"""area of rectangle"""
+
+#l=int(input("enter the length: "))
+#w=int(input("enter the width: "))
+#def area(l,w):
+#    return l*w
+#area=area(l,w)
+#print("area=",area)
+
+"""sum"""
+#a=int(input("enter the number: "))
+#b=int(input("enter the number: "))
+#def sum(a,b):
+#    return a+b
+#sum=sum(a,b)
+#print("sum=",sum)
+
+"""area of circle"""
+#r=int(input("enter the radius: "))
+#def area(r):
+#    return 3.14*r**2
+#area=area(r)
+#print("area=",area)
+
+
+"""even or odd"""
+#n=int(input("enter a number: "))
+#def even(n):
+#    return n%2
+#t=even(n)
+#if t==0:
+#    print("true")
+#else:
+#   print("false")
+
+
+"""largest number"""
+ls=[2,4,5,8,10.20.7,1]
+#def find_max(ls):
+#    return max(ls)
+#max=find_max(a,b,c)
+#if max==a:
+#    print("largest number is: ",a)
+#elif max==b:
+#    print("largest number is: ",b)
+#else:
+#    print("largest number is: ",c)
+
+    
+
+"""discount"""
+a=int(input("enter the amount: "))
+d=int(input("enter the discount: "))
+def discount(disc):
+    return (a/d)*100
+amount=discount(disc)   
