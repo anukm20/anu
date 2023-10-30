bcfe3f277661d27dbf0f6c831c8860e9d9311983
anukm20 <143874513+anukm20@users.noreply.github.com>
2023-10-30 09:30:43 +0530

    Add files via upload

---
+++ oop class.py
@@ -0,0 +1,48 @@
+""""""
+# class vehicle:
+#     door=5
+#     wheel=4
+#     def start(self):
+#         print("engine start")
+# car=vehicle()
+# car.start()
+
+
+
+
+class vehicle:
+     door=5
+     wheel=4
+     def start(self,a,b):
+         print(a+b,"engin start")
+car=vehicle() 
+car.start(20,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
