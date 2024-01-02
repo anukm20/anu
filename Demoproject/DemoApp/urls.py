393474b507b0c2674845de79fe2e58f46206c6be
anurag <anuanurag248@gmail.com>
2024-01-02 09:10:36 +0530

    file added

--- Demoproject/DemoApp/urls.py
+++ Demoproject/DemoApp/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.Home,name='Home')
+    path('',views.Home,name='Home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
 ]
